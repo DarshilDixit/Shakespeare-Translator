f47e51b2d41f28efec41034d011602d1b32dab13
Darshil Dixit <this.is.darshil@gmail.com>
2021-09-23 15:32:29 +0530

    chore

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var textInput = document.querySelector("#txt-input");
 var textOutput = document.querySelector("#txt-output");
 var serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
 
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
 
+function errorHandler(error) {
+    console.log("Error occured", error);
+    alert("Sorry,Something went wrong with the server. Try again after sometime.")
+}
 
-
-function clickHandler(){
-    
+function clickHandler() {
+    fetch(getTranslationURL(textInput.value))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            textOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
 
 
