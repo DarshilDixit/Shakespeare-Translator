9880f6c4c67fa3e9ad056295b25e87759aafdc16
Darshil Dixit <this.is.darshil@gmail.com>
2021-09-23 15:19:41 +0530

    chore

---
+++ app.js
@@ -0,0 +1,14 @@
+var buttonTranslate = document.querySelector("#btn-trans");
+var textInput = document.querySelector("#txt-input");
+var textOutput = document.querySelector("#txt-output");
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
+
+
+
+
+function clickHandler(){
+    
+}
+
+
+buttonTranslate.addEventListener("click", clickHandler);
